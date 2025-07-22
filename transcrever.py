8561d12ed0f5005aa1d92d0c2eb19075588c3924
JeffersonDK <127211299+JeffersonDK@users.noreply.github.com>
2025-07-22 17:38:18 -0300

    Update transcrever.py

--- transcrever.py
+++ transcrever.py
@@ -30,7 +30,7 @@ genai.configure(api_key="AIzaSyCDsvN4QtsDE2Bi1grncPEwDIAY96e4sCE")
 # Configurar o modelo com instruções do Gem
 model1 = genai.GenerativeModel(
     model_name='gemini-2.0-flash',
-    system_instruction="-comporte-se como um assistente para pesquisar o que for perguntado.Pergunte em que pode ajudar.cumprimente-a pessoa  pelo nome , se ela disser o nome. -Não pronuncie a palavra asterisco na resposta em forma de lista."
+    system_instruction="-comporte-se como um assistente para pesquisar o que for perguntado.cumprimente a pessoa  pelo nome , se ela disser o nome e pergunte em que pode ajudar. -Não gere asterisco na resposta em forma de lista nem  nenhum outro caractere, separe os itens com ponto-e-virgula ao final.Preferencia: os itens ficam numa mesma linha. "
 )
 
 # Captura de áudio
